79c8a79657254ff1f8a55ddbfc9c38de0a6c1e4f
kosta <kosta@DESKTOP-VEOB44H>
2024-01-23 15:23:46 +0900

    dsfgfd

---
+++ 0123/src/VariableTest.java
@@ -0,0 +1,22 @@
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		boolean isMarried = false;
+		char bloodType = 'B';
+		int age = 24;
+		double height = 166.5;
+		float weight = 99.5f;
+//		byte b = 128;  // overflow (byte는 127까지 저장 가능)
+		String name = "황한샘";
+		
+		System.out.println(isMarried);
+		System.out.println(bloodType);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		System.out.println(name);
+		
+	}
+
+}
