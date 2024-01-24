f68cc99995d40e828646dd2a42b9291b1aa853fe
HHansaem <hhs01071331@gmail.com>
2024-01-24 12:43:00 +0900

    for테스트

---
+++ 0124/src/ForTest3.java
@@ -0,0 +1,55 @@
+
+public class ForTest3 {
+	public static void main(String[] args) {
+		
+		//*****
+//		for(int i=0; i<5; i++) {
+//				System.out.print("*");
+//		}
+		
+		
+		//*****
+		//*****
+		//*****
+		//*****
+		//*****
+//		for(int i=0; i<5; i++) {
+//			for(int j=0; j<5; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		//*
+		//**
+		//***
+		//****
+		//*****
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		//*****
+		//****
+		//***
+		//**
+		//*
+		for(int i=0; i<5; i++) {
+			for(int j=5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+
+	
+	
+	
+	}
+}
