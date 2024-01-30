8e03834c80b794f958fbedb23892b4397fbd7cf6
HHansaem <hhs01071331@gmail.com>
2024-01-30 11:49:04 +0900

    class 예제

---
+++ 0130/src/ClassTest2.java
@@ -0,0 +1,33 @@
+class Account {
+	String id, name;
+	int balance;
+	
+	void deposit(int money) {
+		balance += money;
+	}
+	
+	void withdraw(int money) {
+		balance -= money;
+	}
+	
+	String info() {
+		return "계좌번호:" + id + ", 이름:" + name + ", 잔액:" + balance; 
+	}
+}
+
+public class ClassTest2 {
+	public static void main(String[] args) {
+		Account acc = new Account();
+		acc.id = "10001";
+		acc.name = "홍길동";
+		acc.balance = 100000;
+		System.out.println(acc.info());
+		acc.deposit(10000);
+		System.out.println(acc.info());
+		acc.withdraw(5000);
+		System.out.println(acc.info());
+	}
+}
+//계좌번호:10001, 이름:홍길동, 잔액:100000
+//계좌번호:10001, 이름:홍길동, 잔액:110000
+//계좌번호:10001, 이름:홍길동, 잔액:105000
