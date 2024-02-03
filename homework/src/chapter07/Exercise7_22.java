bc9e602ac0306a9ab467313df8a67b7da9e4d966
HHansaem <hhs01071331@gmail.com>
2024-02-04 02:56:05 +0900

    커밋확인용

--- homework/src/chapter07/Exercise7_22.java
+++ homework/src/chapter07/Exercise7_22.java
@@ -109,5 +109,6 @@ public class Exercise7_22 {
 	public static void main(String[] args) {
 		Shape[] arr = {new Circle(5.0), new Rectangle(3,4), new Circle(1)};
 		System.out.println("면적의 합:"+sumArea(arr));
+		System.out.println("컷밋");
 	}
 }
