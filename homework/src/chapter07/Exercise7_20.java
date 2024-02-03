0e58d08abc6053cdd30e1994bfed1d146a675d82
HHansaem <hhs01071331@gmail.com>
2024-02-04 02:24:32 +0900

    ch7연습문제-캐스팅

--- homework/src/chapter07/Exercise7_20.java
+++ homework/src/chapter07/Exercise7_20.java
@@ -6,8 +6,8 @@ public class Exercise7_20 {
 	public static void main(String[] args) {
 		Parent1 p = new Child1();  //업캐스팅
 		Child1 c = new Child1();
-		System.out.println("p.x = " + p.x);  //100
-		p.method();  //Child Method
+		System.out.println("p.x = " + p.x);  //100 -> 부모타입이니까 (오버라이딩을 제외하고, 타입 우선)
+		p.method();  //Child Method -> 오버라이딩된 메서드기 때문에 new한 자식의 메서드 호출
 		System.out.println("c.x = " + c.x);  //200
 		c.method();  //Child Method
 	}
