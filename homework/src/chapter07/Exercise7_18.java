a94e09de72367bbd4bd7b7b278d93047a18261e3
HHansaem <hhs01071331@gmail.com>
2024-02-05 12:32:54 +0900

    ch7연습문제-18(수정)

--- homework/src/chapter07/Exercise7_18.java
+++ homework/src/chapter07/Exercise7_18.java
@@ -1,7 +1,7 @@
 package chapter07;
 
 //다음과 같은 실행결과를 얻도록 코드를 완성하시오.
-//[Hint] instanceof연산자를 사용해서 형변환한다. 메서드명 : action
+//[Hint] instanceOf연산자를 사용해서 형변환한다. 메서드명 : action
 // 기 능 : 주어진 객체의 메서드를 호출한다.
 // DanceRobot인 경우, dance()를 호출하고,
 // SingRobot인 경우, sing()을 호출하고,
@@ -11,18 +11,18 @@ package chapter07;
 
 public class Exercise7_18 {
 	
-	//instanceof 연산자
+	//instanceOf 연산자
 	//객체 타입을 확인하는 연산자
 	//형변환 가능 여부를 확인하며 true / false로 결과를 반환
 	//주로 상속 관계에서 부모객체인지 자식객체인지 확인하는 데 사용
-	//"객체 instanceof 클래스" 를 선언함으로써 사용
-	static void action(Robot r) {
-		if(r instanceof DanceRobot) {  //DanceRobot으로 형변환이 가능하면
-			((DanceRobot) r).dance();  //형변환 후 메서드 호출
-		} else if(r instanceof SingRobot) {
-			((SingRobot) r).sing();
-		} else if(r instanceof DrawRobot) {
-			((DrawRobot) r).draw();
+	//"객체 instanceOf 클래스" 를 선언함으로써 사용
+	static void action(Robot robot) {
+		if(robot instanceof DanceRobot) {  //DanceRobot으로 형변환이 가능하면
+			((DanceRobot) robot).dance();  //형변환 후 메서드 호출
+		} else if(robot instanceof SingRobot) {
+			((SingRobot) robot).sing();
+		} else if(robot instanceof DrawRobot) {
+			((DrawRobot) robot).draw();
 		}
 	}
 	
