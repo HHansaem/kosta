3c0d51fd8c94623b82d1938cb31af6949b9d0df9
HHansaem <hhs01071331@gmail.com>
2024-02-06 11:24:53 +0900

    Exception예제

--- 0206/src/ExceptionTest2.java
+++ 0206/src/ExceptionTest2.java
@@ -1,20 +1,23 @@
 
 public class ExceptionTest2 {
 	public static void main(String[] args) {
-		int[] arr1 = {10, 20, 30, 40};
+		int[] arr1 = {10, 20, 30, 40, 50};
 		int[] arr2 = {2, 0, 6, 8};
 		
 		for(int i = 0; i < arr1.length; i++) {
 			try {
 				System.out.println(arr1[i] / arr2[i]);
-			} catch (Exception e) {
-				System.out.println("by zero");
+			} catch (ArithmeticException e) {  //예외 종류에 따라 실행 결과 다르게
+				System.out.println(e.getMessage());
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println(0);
 			}
 		}
 		
 	}
 }
 //5
-// by zero
+// / by zero
 //5
 //5
+//0
