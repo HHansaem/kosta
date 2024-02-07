145aa0612c851712594fddc585af5ed690132569
HHansaem <hhs01071331@gmail.com>
2024-02-07 12:00:16 +0900

    Object equals 오버라이딩 예제

--- 0207/src/ObjectTest1.java
+++ 0207/src/ObjectTest1.java
@@ -1,28 +1,4 @@
-class Person {
-	int age;
-	String name;
-	public Person(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {  //논리적으로 데이터가 같으면 true 다르면 false
-		if(!(obj instanceof Person)) return false;
-		Person per = (Person)obj;
-		return name.equals(per.name) && age == per.age;
-	}
-	
-	@Override
-	public int hashCode() {
-		return name.hashCode() + age*2;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("이름:%s, 나이:%d", name, age);
-	}
-}
+import pac.Person;
 
 public class ObjectTest1 {
 	
