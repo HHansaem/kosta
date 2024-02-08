76495b23bc3103268529783e32c9abf1aeaa6626
HHansaem <hhs01071331@gmail.com>
2024-02-08 10:54:53 +0900

    ArrayList 예제

---
+++ 0208/src/ArrayListTest1.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+class Person {
+	String name;
+	int age;
+	
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String info() {
+		return "이름:" + name  + ", 나이:" + age;
+	}
+}
+
+public class ArrayListTest1 {
+	public static void main(String[] args) {
+//		<Person> -> 제네릭(generic) : ArrayList 안에 데이터를 Person으로 넣을 거야 (데이터 타입 일반화.. 미리 지정)
+		ArrayList<Person> list = new ArrayList<>();  //ArrayList 객체 생성
+		list.add(new Person("hong", 20));  //add()로 ArrayList에 데이터 넣어주기
+		list.add(new Person("song", 30));
+		list.add(new Person("gong", 35));
+		
+//		for(int i = 0; i < list.size(); i++) {
+//			System.out.println(list.get(i));  //get()으로 ArrayList의 데이터 불러오기
+//		}
+		
+		for(Person p : list) {  //for-each문(향상된 for문)
+			System.out.println(p.info());  //제네릭 쓰면 여기서 Object말고 Person써도 다운캐스팅 안 해도 됨.
+		}
+	}
+}
