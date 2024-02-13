6ca834b554dce796ca207019220bea0b253b361c
HHansaem <hhs01071331@gmail.com>
2024-02-13 11:30:04 +0900

    FileInputStream 예제

---
+++ 0213/src/FileInputStreamTest1.java
@@ -0,0 +1,33 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileInputStreamTest1 {
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		
+		try {
+			fis = new FileInputStream("news.txt");  //파일에서 byte단위의 데이터 읽어오기
+			fos = new FileOutputStream("news2.txt");  //파일에 byte단위의 데이터 생성 후 저장
+			
+			byte[] buff = new byte[10];
+			int len;
+			
+			//read : 파일에서 최대 buff의 크기만큼 데이터를 읽고, 실제로 읽은 바이트 수를 반환 
+			//(여기선 반복문 10번.. 읽을 데이터가 없으면 -1 반환)
+			while((len = fis.read(buff)) > 0) {  
+				fos.write(buff, 0, len);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fis != null) fis.close();
+				if(fos != null) fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
