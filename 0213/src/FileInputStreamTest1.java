7ec1434e88c26be167573331d4549d7f9165c353
HHansaem <hhs01071331@gmail.com>
2024-02-13 11:48:04 +0900

    FileInputStream 예제

--- 0213/src/FileInputStreamTest1.java
+++ 0213/src/FileInputStreamTest1.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 
 public class FileInputStreamTest1 {
 	public static void main(String[] args) {
+		
+		if(args.length < 2) return;
+		String readFile = args[0];
+		String writeFile = args[1];
+		
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
 		
 		try {
+			//main의 파라미터로 받아서 사용 (프로젝트 우클릭 -> Run as -> Run Configuration)
+//			fis = new FileInputStream(readFile);  
+//			fos = new FileOutputStream(writeFile);
 			fis = new FileInputStream("news.txt");  //파일에서 byte단위의 데이터 읽어오기
 			fos = new FileOutputStream("news2.txt");  //파일에 byte단위의 데이터 생성 후 저장
 			
